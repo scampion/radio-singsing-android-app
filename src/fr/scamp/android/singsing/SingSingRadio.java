baefb2426536b5dfcaa5825558d350a368c21269
Sebastien Campion <seb@scamp.fr>
2011-05-11 17:37:29 +0200

    1.2

--- src/fr/scamp/android/singsing/SingSingRadio.java
+++ src/fr/scamp/android/singsing/SingSingRadio.java
@@ -10,9 +10,6 @@ import android.view.View;
 import android.view.View.OnClickListener ;
 import android.widget.Button; 
 import android.widget.TextView; 
-import java.net.URL;
-import java.net.URLConnection;
-import fr.scamp.android.singsing.IcyStreamMeta; 
 
 import java.io.IOException;
 
@@ -36,7 +33,7 @@ public class SingSingRadio extends Activity implements OnClickListener
     b = (Button)findViewById(R.id.quit);
     b.setOnClickListener(this);
 
-    status = (TextView)findViewById(R.id.status);
+    //status = (TextView)findViewById(R.id.status);
   }
 
   public void onClick(View v) {
@@ -57,62 +54,9 @@ public class SingSingRadio extends Activity implements OnClickListener
       mp.setDataSource(url);
       mp.prepare();
       mp.start();
-      new HTTPMetadataTask().execute();
     } catch (IOException e) {
       Log.e(LOG_TAG, "Mediaplayer failed");
     }
   }
-
-  private class HTTPMetadataTask extends AsyncTask<Void, Void, String> {
-    protected String pochette_php = "http://www.sing-sing.org/programmation/pochette_frame2.php" ; 
-
-    protected String doInBackground(Void... params) {
-      try {
-	URL u = new URL(pochette_php);
-	//URLConnection uconn = u.openConnection();
-	//uconn.connect();
-	return ""+u.getContent(); 
-      } 
-      catch (Exception e) {
-	Log.e(LOG_TAG, "Cover retrieve failed "+e.getMessage());
-	return "No cover" ;
-      }
-    } 
-    
-    protected void onPostExecute(String result) {
-      TextView status = (TextView)findViewById(R.id.status);
-      status.setText(result);    
-    }
-  }
-
-  private class MetadataTask extends AsyncTask<URL, Void, IcyStreamMeta> {
-    protected IcyStreamMeta streamMeta;
-    protected IcyStreamMeta doInBackground(URL... urls) {
-
-      //DEBUG
-      //TextView status = (TextView)findViewById(R.id.status);
-      //status.setText("Debug"+urls[0]);
-
-      streamMeta = new IcyStreamMeta(urls[0]);
-      try {
-	streamMeta.refreshMeta();
-      } catch (IOException e) {
-	Log.e(MetadataTask.class.toString(), e.getMessage());
-      }
-      return streamMeta;
-    }
-    
-    protected void onPostExecute(IcyStreamMeta result) {
-      try {
-	//TextView status = (TextView)findViewById(R.id.status);
-	String t = streamMeta.getArtist();
-	//status.setText(t+" done");
-
-      } catch (IOException e) {
-	Log.e(MetadataTask.class.toString(), e.getMessage());
-      }
-    }
-    
-  }
-
 }
+
