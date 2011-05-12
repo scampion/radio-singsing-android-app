844b7f83f1b9ed310910316aeef6a4ff896855bc
Sebastien Campion <seb@scamp.fr>
2011-05-12 14:50:40 +0200

    version 1.3

--- src/fr/scamp/android/singsing/SingSingRadio.java
+++ src/fr/scamp/android/singsing/SingSingRadio.java
@@ -39,9 +39,11 @@ public class SingSingRadio extends Activity implements OnClickListener, OnInfoLi
 {
   private static final String LOG_TAG = "SingSingRadio";
   private MediaPlayer mp = new MediaPlayer();
-  private TextView artist, title  ; 
-  private ImageView cover ; 
+  private String url = "http://stream.sing-sing.org:8000/singsing24" ;
   private String metadata_url = "http://www.sing-sing.org/navi/titre/actuellement.xml" ;
+
+  protected String artist, title ;
+  protected Bitmap cover_bitmap ;
 
   @Override
     public void onCreate(Bundle savedInstanceState)
@@ -54,27 +56,33 @@ public class SingSingRadio extends Activity implements OnClickListener, OnInfoLi
     b.setOnClickListener(this);
     b = (Button)findViewById(R.id.high);
     b.setOnClickListener(this);
+    b = (Button)findViewById(R.id.refresh);
+    b.setOnClickListener(this);
     b = (Button)findViewById(R.id.exit);
     b.setOnClickListener(this);
 
-    artist = (TextView)findViewById(R.id.artist);
-    title = (TextView)findViewById(R.id.title);
-    cover = (ImageView) findViewById(R.id.cover);   
   }
 
   public void onClick(View v) {
-    String url = "http://stream.sing-sing.org:8000/singsing24" ;
     switch(v.getId()) {
     case R.id.low :
       url = "http://stream.sing-sing.org:8000/singsing24" ;
+      play();
       break ;
     case R.id.high :
       url = "http://stream.sing-sing.org:8000/singsing128" ;
+      play();
+      break ;
+    case R.id.refresh :      
+      new UpdateMetadata().execute();
       break ;
     case R.id.exit :
       Process.killProcess(Process.myPid());
     }
-    update_metadata();
+  }
+
+  private void play(){
+    new UpdateMetadata().execute();
     try {
       mp.stop();
       mp.reset();
@@ -90,102 +98,112 @@ public class SingSingRadio extends Activity implements OnClickListener, OnInfoLi
   }
 
   public boolean onInfo(MediaPlayer mp, int what, int extra){
-    Log.i(LOG_TAG, "MEDIA_INFO "+what);
+    Log.d(LOG_TAG, "MEDIA_INFO "+what);
     if ( what == mp.MEDIA_INFO_METADATA_UPDATE){
-      update_metadata();
+      new UpdateMetadata().execute();
       Log.i(LOG_TAG, "MEDIA_INFO_METADATA_UPDATE");
     }    
     return true;
   }
 
   public void onBufferingUpdate(MediaPlayer mp, int percent){
-    Log.i(LOG_TAG, "Buffered "+percent+"%");
+    Log.d(LOG_TAG, "Buffered "+percent+"%");
   }
 
   public void onCompletion(MediaPlayer arg0) {
     Log.d(LOG_TAG, "onCompletion called");
   }
-
-  
-  private void update_metadata()
-  {
-    InputStream in = null;
-    try {
-      in = OpenHttpConnection(metadata_url);
-      Document doc = null;
-      DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-      DocumentBuilder db;            
+   
+  private class UpdateMetadata extends AsyncTask<Void, Void, Void> {
+    protected Void doInBackground(Void... params) {
+      Log.d(LOG_TAG, "update metadata");
+      InputStream in = null;
       try {
-	db = dbf.newDocumentBuilder();
-	doc = db.parse(in);
-      } catch (ParserConfigurationException e) {
-	Log.e(LOG_TAG, "Config XML Parser error "+e);
-      } catch (SAXException e) {
-	Log.e(LOG_TAG, "SAX XML Parser error "+e);
-      }           
-      doc.getDocumentElement().normalize();             
+	in = OpenHttpConnection(metadata_url);
+	Document doc = null;
+	DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+	DocumentBuilder db;            
+	try {
+	  db = dbf.newDocumentBuilder();
+	  doc = db.parse(in);
+	} catch (ParserConfigurationException e) {
+	  Log.e(LOG_TAG, "Config XML Parser error "+e);
+	} catch (SAXException e) {
+	  Log.e(LOG_TAG, "SAX XML Parser error "+e);
+	}           
+	doc.getDocumentElement().normalize();             
 
-      NodeList itemNodes = doc.getElementsByTagName("artist"); 
-      Element element = (Element) doc.getElementsByTagName("artist").item(0);
-      artist.setText(element.getFirstChild().getNodeValue()+" - ");
+	NodeList itemNodes = doc.getElementsByTagName("artist"); 
+	Element element = (Element) doc.getElementsByTagName("artist").item(0);
+	//artist.setText(element.getFirstChild().getNodeValue()+" - ");
+	artist = element.getFirstChild().getNodeValue();
 
-      itemNodes = doc.getElementsByTagName("title"); 
-      element = (Element) doc.getElementsByTagName("title").item(0);
-      title.setText(element.getFirstChild().getNodeValue());
+	itemNodes = doc.getElementsByTagName("title"); 
+	element = (Element) doc.getElementsByTagName("title").item(0);
+	//title.setText(element.getFirstChild().getNodeValue());
+	title = element.getFirstChild().getNodeValue();
 
-      itemNodes = doc.getElementsByTagName("cover"); 
-      element = (Element) doc.getElementsByTagName("cover").item(0);
-      String cover_url = element.getFirstChild().getNodeValue();
-      Bitmap bitmap = DownloadImage("http://"+cover_url) ;
-      cover.setImageBitmap(bitmap);     
-    } catch (IOException e) {
+	itemNodes = doc.getElementsByTagName("cover"); 
+	element = (Element) doc.getElementsByTagName("cover").item(0);
+	String cover_url = element.getFirstChild().getNodeValue();
+	cover_bitmap = DownloadImage("http://"+cover_url) ;
+      } catch (IOException e) {
 	Log.e(LOG_TAG, "update metadata error "+e);     
-    }
-  }
-
-  private Bitmap DownloadImage(String URL)
-  {        
-    Bitmap bitmap = null;
-    InputStream in = null;        
-    try {
-      in = OpenHttpConnection(URL);
-      bitmap = BitmapFactory.decodeStream(in);
-      in.close();
-    } catch (IOException e) {
-      Log.e(LOG_TAG, "Download Image error "+e);
-    }
-    return bitmap;                
-  }
-
-  private InputStream OpenHttpConnection(String urlString) 
-    throws IOException
-  {
-    InputStream in = null;
-    int response = -1;               
-    URL url = new URL(urlString); 
-    URLConnection conn = url.openConnection();                 
-    if (!(conn instanceof HttpURLConnection))                     
-      throw new IOException("Not an HTTP connection");        
-    try{
-      HttpURLConnection httpConn = (HttpURLConnection) conn;
-      httpConn.setAllowUserInteraction(false);
-      httpConn.setInstanceFollowRedirects(true);
-      httpConn.setRequestMethod("GET");
-      httpConn.connect(); 
-
-      response = httpConn.getResponseCode();                 
-      if (response == HttpURLConnection.HTTP_OK) {
-	in = httpConn.getInputStream();                                 
-      }                     
-    }
-    catch (Exception ex)
-      {
-	throw new IOException("Error connecting");            
       }
-    return in;     
+      return null;
+    }
+    
+    protected void onPostExecute(Void param) {
+      TextView tv ; 
+      tv = (TextView)findViewById(R.id.artist);
+      tv.setText(artist+" - ");
+      tv = (TextView)findViewById(R.id.title);
+      tv.setText(title);
+      ImageView cover = (ImageView) findViewById(R.id.cover);   
+      cover.setImageBitmap(cover_bitmap);     
+    }
+    private Bitmap DownloadImage(String URL)
+    {        
+      Bitmap bitmap = null;
+      InputStream in = null;        
+      try {
+	in = OpenHttpConnection(URL);
+	bitmap = BitmapFactory.decodeStream(in);
+	in.close();
+      } catch (IOException e) {
+	Log.e(LOG_TAG, "Download Image error "+e);
+      }
+      return bitmap;                
+    }
+
+    private InputStream OpenHttpConnection(String urlString) 
+      throws IOException
+    {
+      InputStream in = null;
+      int response = -1;               
+      URL url = new URL(urlString); 
+      URLConnection conn = url.openConnection();                 
+      if (!(conn instanceof HttpURLConnection))                     
+	throw new IOException("Not an HTTP connection");        
+      try{
+	HttpURLConnection httpConn = (HttpURLConnection) conn;
+	httpConn.setAllowUserInteraction(false);
+	httpConn.setInstanceFollowRedirects(true);
+	httpConn.setRequestMethod("GET");
+	httpConn.connect(); 
+
+	response = httpConn.getResponseCode();                 
+	if (response == HttpURLConnection.HTTP_OK) {
+	  in = httpConn.getInputStream();                                 
+	}                     
+      }
+      catch (Exception ex)
+	{
+	  throw new IOException("Error connecting");            
+	}
+      return in;     
+    }
+
   }
-
 }
-
-
 
